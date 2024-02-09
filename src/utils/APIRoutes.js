3fd3e89856d1da95d42c9b57a73deef12bda97ee
Nandeesh P S <nandish0602@gmail.com>
2024-02-09 22:45:19 +0530

    last commit

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,12 +1,9 @@
 
-// export const host = "http://localhost:5000";
+export const host = "http://localhost:5000";
 
-export const host = "https://chatapplicationbackend-9tef.onrender.com";
-
-
+// export const host = "https://chat-application-backend-d100bq4gd-nandeeshps-projects.vercel.app";
 
 export const loginRoute = `${host}/api/auth/login`;
-
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
